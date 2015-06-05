1dcc6ddcd1ca37d782cecd51796fa7deb4df5c93
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-05 19:23:05 +0100

    update

--- src/pls/Startup.cs
+++ src/pls/Startup.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 
 namespace pls
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+		public static IConfiguration Configuration { get; set; }
+		public Startup(IHostingEnvironment env)
+		{
+			Configuration = new Configuration()
+				.AddJsonFile("config.json");
+		}
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
         {
         }
 
