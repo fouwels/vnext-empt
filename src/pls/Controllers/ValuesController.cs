ab4bfd10faf24bd575fae409c2853f4dd2d38dd2
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-12 12:44:11 +0100

    update

---
+++ src/pls/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pls.Controllers
+{
+    [Route("api/v0/[controller]/[action]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> test()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}S
--- src/pls/Startup.cs
+++ src/pls/Startup.cs
@@ -6,29 +6,41 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.ConfigurationModel.UserSecrets;
 using Microsoft.Framework.DependencyInjection;
 
 namespace pls
 {
     public class Startup
     {
-		public static IConfiguration Configuration { get; set; }
+		public IConfiguration Configuration { get; set; }
 		public Startup(IHostingEnvironment env)
 		{
-			Configuration = new Configuration()
-				.AddJsonFile("config.json");
+			var conf = new Configuration();
+
+			conf.AddJsonFile("config.json");
+			if (env.IsEnvironment("Development")) { conf.AddUserSecrets(); }
+
+			Configuration = conf;
 		}
-		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			app.UseMvc(); //attribute routing
+			app.UseStaticFiles();
+
+			//app.UseMvc(routes =>
+			//{
+			//	routes.MapRoute(
+			//	name: "default",
+			//	template: "{controller}/{action}/{id?}"
+			//	//, defaults: new { controller = "Home", action = "Index" }
+			//	);
+			//});
+		}
+	}
 }
